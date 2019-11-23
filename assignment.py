bf13ee299907ae03cd1069e40d1a729bcb1cc947
Daniel Nam <dnam@daniels-mbp-2.devices.brown.edu>
2019-11-22 21:55:11 -0500

    basic layout

---
+++ assignment.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+import preprocess
+
+def main()
+
+if __name__ = '__name__':
+    main()
---
+++ preprocess.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+
+def get_data(train_file, test_file):
+
+    return train_data, train_label, test_data, test_labels
