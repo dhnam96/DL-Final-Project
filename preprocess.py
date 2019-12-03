0e24cf1d7af81f084347f5bff4d5e6a476c0b1e0
Daniel Nam <dnam@daniels-mbp-2.devices.brown.edu>
2019-12-02 21:27:01 -0500

    car data changes

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,18 @@
 import numpy as np
 import tensorflow as tf
 import os, os.path
+import glob
+from PIL import Image
 
-def get_data(train_file, test_file):
+def get_data():
 
-    image_list = []
-    test_path = "/stanford-cars-dataset/cars_test"
-    train_path = "/stanford-cars-dataset/cars_train"
+    test_images = []
+    test_path = "/stanford-cars-dataset/cars_test/*.jpg"
+    train_path = "/stanford-cars-dataset/cars_train/*.jpg"
     valid_images = [".jpg"]
-    for image in os.listdir(test_path):
+    for test_file in glob.glob(test_path):
         # TODO: Preprocess
+        image = Image.open(test_file)
+        test_images.append(image)
+
     return train_data, train_label, test_data, test_labels
