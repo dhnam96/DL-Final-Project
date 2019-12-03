9065c3778a5243c34e9bcd65b5fbb881364e630f
Daniel Nam <dnam@daniels-mbp-2.devices.brown.edu>
2019-12-02 21:16:45 -0500

    little changes to preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,13 @@
 import numpy as np
 import tensorflow as tf
+import os, os.path
 
 def get_data(train_file, test_file):
 
+    image_list = []
+    test_path = "/stanford-cars-dataset/cars_test"
+    train_path = "/stanford-cars-dataset/cars_train"
+    valid_images = [".jpg"]
+    for image in os.listdir(test_path):
+        # TODO: Preprocess
     return train_data, train_label, test_data, test_labels
