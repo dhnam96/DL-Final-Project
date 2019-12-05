9c5cf66bcefc120e57b5cf2cb0a819065bdddd8b
Min Jeong Kang <min_jeong_kang@brown.edu>
2019-12-04 20:02:08 -0500

    Preprocess updated

--- assignment.py
+++ assignment.py
@@ -1,8 +1,9 @@
-import os
-import sys
+import os, sys
 import numpy as np
 import tensorflow as tf
 from preprocess import get_data 
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 class Generator_Model(tf.keras.Model):
     def __init__(self):
@@ -13,8 +14,8 @@ class Discriminator_Model(tf.keras.Model):
         super(Discriminator_Model, self).__init__()
 
 def main():
-    train_data = get_data('./cars_train')
-    test_data = get_data('./cars_test')
+    train_data = get_data('./cars_train', True)
+    test_data = get_data('./cars_test', True)
 
 if __name__ == '__main__':
    main()
--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import cv2
-import os
-import shutil
+import cv2, os, shutil, glob 
 from tensorflow.keras.preprocessing import image
-import glob 
 
 def crop_center(image, target_size):
     h, w = image.shape[-3], image.shape[-2]
@@ -15,22 +12,24 @@ def crop_center(image, target_size):
     return tf.image.resize(cropped_image, target_size)
 
 
-def get_data(dir_path):
-    def load_and_process_image(file_path, target_size = (32, 32)):
+def get_data(dir_path, resize=False):
+    def load_and_process_image(file_path, target_size = (256, 256)):
         img = image.load_img(file_path)
         img = image.img_to_array(img)
-        img = crop_center(img, target_size)
-        save_path = os.path.join(os.path.dirname(file_path), 'preprocessed', os.path.basename(file_path))
-        image.save_img(save_path, img)
+        if resize:
+            img = crop_center(img, target_size)
+            save_path = os.path.join(os.path.dirname(file_path), 'preprocessed', os.path.basename(file_path))
+            image.save_img(save_path, img)
         return img
 
     file_path = dir_path + '/*.jpg'
     dataset = glob.glob(file_path)
 
-    preprocess_dir = os.path.join(dir_path, 'preprocessed')
-    if os.path.exists(preprocess_dir):
-        shutil.rmtree(preprocess_dir)
-    os.mkdir(os.path.join(dir_path, 'preprocessed'))
+    if resize:
+        preprocess_dir = os.path.join(dir_path, 'preprocessed')
+        if os.path.exists(preprocess_dir):
+            shutil.rmtree(preprocess_dir)
+        os.mkdir(os.path.join(dir_path, 'preprocessed'))
 
     dataset= np.array([load_and_process_image(img) for img in dataset])
 
