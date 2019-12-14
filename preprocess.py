3e44b94c6188057b864403d6b0113092be1596c6
Daniel Nam <dnam@daniels-mbp-2.devices.brown.edu>
2019-12-13 19:22:53 -0500

    new preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,43 +1,49 @@
+import os, shutil, glob
 import numpy as np
 import tensorflow as tf
-import cv2, os, shutil, glob 
 from tensorflow.keras.preprocessing import image
 
-NUM_DATA = 18000
 
-def crop_center(image, target_size):
+def crop(image, target_size):
     h, w = image.shape[-3], image.shape[-2]
-    if h > w:
-        cropped_image = tf.image.crop_to_bounding_box(image, (h - w) // 2, 0, w, w)
-    else:
-        cropped_image = tf.image.crop_to_bounding_box(image, 0, (w - h) // 2, h, h)
+    cropped_image = tf.image.crop_to_bounding_box(image, int(0.1*h), int(0.1*w), int(0.8*h), int(0.9*w))
     return tf.image.resize(cropped_image, target_size)
 
+def get_data(dir_path, target_size=(250,250), processed=False):
+    processed_dir = os.path.join(os.path.dirname(dir_path), 'processed')
 
-def get_data(dir_path, target_size, resize=False):
-    def load_and_process_image(file_path, index, target_size = (64, 64)):
+    def load_and_process_image(file_path):
         img = image.load_img(file_path)
         img = image.img_to_array(img)
-        if resize:
-            img = crop_center(img, target_size)
-            save_path = os.path.join(os.path.dirname(file_path), 'preprocessed', os.path.basename(file_path))
+        if not processed:
+            img = crop(img, target_size)
+            save_path = os.path.join(processed_dir, os.path.basename(file_path))
             image.save_img(save_path, img)
-        if index % 100 == 0:
-            print("Preprocessing %3.2f percent completed" %(index/NUM_DATA*100))
         return img
 
-    file_path = dir_path + '/*.jpg'
-    dataset = glob.glob(file_path)
+    if not processed:
+        file_path = dir_path + '/*/*.jpg'
+        dataset = glob.glob(file_path)
+        if os.path.exists(processed_dir):
+            shutil.rmtree(processed_dir)
+        os.mkdir(processed_dir)
+    else:
+        file_path = processed_dir + '/*.jpg'
+        dataset = glob.glob(file_path)
 
-    if resize:
-        preprocess_dir = os.path.join(dir_path, 'preprocessed')
-        if os.path.exists(preprocess_dir):
-            shutil.rmtree(preprocess_dir)
-        os.mkdir(os.path.join(dir_path, 'preprocessed'))
+    num_img = len(dataset)
+    data = np.zeros(shape=(num_img, target_size[0], target_size[1], 3), dtype=np.float32)
 
-    data = np.zeros(shape=(len(dataset), target_size[0], target_size[1], 3), dtype=np.float32)
     for (index, img) in enumerate(dataset):
-        img_data = load_and_process_image(img, index)
-        data[index] = img_data / 255   
-    
+        img_data = load_and_process_image(img)
+        data[index] = (img_data / 255 - 0.5) * 2
+        if index % 100 == 0:
+            print("Preprocessing %3.2f percent completed" %(index/num_img*100))
+    print("Preprocessing 100 percent completed")
     return data
+
+def main():
+    get_data('./lfw', target_size=(64,64))
+
+if __name__ == '__main__':
+   main()
