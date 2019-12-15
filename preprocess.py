b6747e5563475de83dcd9344f191fa2121679aa2
Ace-Ma <acejingxiaoma@gmail.com>
2019-12-15 04:58:48 -0500

    Update preprocess

--- preprocess.py
+++ preprocess.py
@@ -3,26 +3,37 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 
-
-def crop(image, target_size):
+def crop_center(image, target_size):
     h, w = image.shape[-3], image.shape[-2]
-    cropped_image = tf.image.crop_to_bounding_box(image, int(0.2*h), int(0.2*w), int(0.6*h), int(0.6*w))
+    if h > w:
+        cropped_image = tf.image.crop_to_bounding_box(image, (h - w) // 2, 0, w, w)
+    else:
+        cropped_image = tf.image.crop_to_bounding_box(image, 0, (w - h) // 2, h, h)
     return tf.image.resize(cropped_image, target_size)
 
+# def crop(image, target_size):
+    # h, w = image.shape[-3], image.shape[-2]
+    # cropped_image = tf.image.crop_to_bounding_box(image, int(0.2*h), int(0.2*w), int(0.6*h), int(0.6*w))
+    # return tf.image.resize(cropped_image, target_size)
+
 def get_data(dir_path, target_size=(250,250), processed=False):
-    processed_dir = os.path.join(os.path.dirname(dir_path), 'processed')
+    
+    if not processed:
+        processed_dir = os.path.join(os.path.dirname(dir_path), 'processed')
+    else:
+        processed_dir = dir_path
 
     def load_and_process_image(file_path):
         img = image.load_img(file_path)
         img = image.img_to_array(img)
         if not processed:
-            img = crop(img, target_size)
+            img = crop_center(img, target_size)
             save_path = os.path.join(processed_dir, os.path.basename(file_path))
             image.save_img(save_path, img)
         return img
 
     if not processed:
-        file_path = dir_path + '/*/*.jpg'
+        file_path = dir_path + '/*.jpg'
         dataset = glob.glob(file_path)
         if os.path.exists(processed_dir):
             shutil.rmtree(processed_dir)
